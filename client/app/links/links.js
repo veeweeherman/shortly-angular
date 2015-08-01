2bf6778ee354b8dce80c8414ef4551429d15fedc
TGA-Student <tga.student@hackreactor.com>
2015-07-31 17:26:42 -0700

    passing 3 tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,10 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getLinks = function() {};
+	Links.getLinks()
+  	.then(function(url){
+  		$scope.data.links = url;
+	  })
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,21 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(resp) {
+      return resp.data;
+    })
+  }
+  return {
+    getLinks: getLinks
+  };
 })
+
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
